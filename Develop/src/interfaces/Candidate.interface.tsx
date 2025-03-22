9fbf41ea6e2c303721ea86d28611d6ee0b43e5e8
ngin2894 <nataliegindraux@gmail.com>
2025-03-22 09:18:33 -0500

    updated the Candidate interface file with the appropriate typescript values

--- Develop/src/interfaces/Candidate.interface.tsx
+++ Develop/src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,15 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+export interface Candidate {
+    avatar_url: string;
+    login: string;
+    id: number;
+}
+
+export default interface CandidateDetails extends Candidate {
+    name: string;
+    location: string;
+    email: string;
+    company: string;
+    html_url: string;
+    bio: string;
+}
