9d7b68a7b71ee9756f562fc072d524a408b65f7c
ngin2894 <nataliegindraux@gmail.com>
2025-03-22 09:24:37 -0500

    Added details to the CandidateCard.tsx file to appropriately display the information for the candidates for the user

--- Develop/src/components/CandidateCard.tsx
+++ Develop/src/components/CandidateCard.tsx
@@ -1,1 +1,29 @@
+import react from 'react';
+import CandidateDetails from '../interfaces/Candidate.interface';
 
+interface CandidateCardProps {
+    candidate: CandidateDetails;
+    onAdd: () => void;
+    onSkip: () => void;
+}
+
+const CandidateCard: React.FC<CandidateCardProps> = ({ candidate, onAdd, onSkip }) => {
+    return (
+        <div>
+            <img src={candidate.avatar_url} alt={candidate.login} width="100" height="100" />
+            <h2>{candidate.login}</h2>
+            <p>{candidate.location}</p>
+            <p>{candidate.email || 'No email available'}</p>
+            <p>{candidate.company || 'No company available'}</p>
+            <a href={candidate.html_url} target="_blank" rel="noopener noreferrer">
+                View Profile
+            </a>
+            <div>
+                <button onClick={onAdd}>Save</button>
+                <button onClick={onSkip}>Skip</button>
+            </div>
+        </div>
+    );
+};
+
+export default CandidateCard;
